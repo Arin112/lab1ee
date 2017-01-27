560663aaac8008f1847d68469e350aabe12b5633
Arin112 <denis.protva@mail.ru>
2017-01-27 20:03:26 +0300

    lab 5-8

---
+++ lab5/main.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <iostream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+#include <termios.h>
+
+using namespace std;
+
+pthread_mutex_t mutex_ = PTHREAD_MUTEX_INITIALIZER;
+
+int wfd, rfd;
+
+
+char getch(){
+
+	char buf=0;
+	struct termios old={0};
+	fflush(stdout);
+	if(tcgetattr(0, &old)<0)
+		perror("tcsetattr()");
+	old.c_lflag&=~ICANON;
+	old.c_lflag&=~ECHO;
+	old.c_cc[VMIN]=1;
+	old.c_cc[VTIME]=0;
+	if(tcsetattr(0, TCSANOW, &old)<0)
+		perror("tcsetattr ICANON");
+	if(read(0,&buf,1)<0)
+		perror("read()");
+	old.c_lflag|=ICANON;
+	old.c_lflag|=ECHO;
+	if(tcsetattr(0, TCSADRAIN, &old)<0)
+		perror ("tcsetattr ~ICANON");
+	//printf("%c\n",buf);
+	return buf;
+}
+
+char * getTime(){
+	time_t current_time;
+	struct tm * time_info;
+	static char timeString[9];  // space for "HH:MM:SS\0"
+
+	time(&current_time);
+	time_info = localtime(&current_time);
+
+	strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info);
+	return timeString;
+}
+
+
+void clearStr(char* str, int len){
+	for(int i=0;i<len;i++){
+		str[i]=0;
+	}
+	return;
+}
+
+bool readLine(int fd, char *str){
+	char ch=0;
+	for(;read(fd, &ch, 1)!=0;str++){
+		if(ch=='\n')return true;
+		*str=ch;
+	}
+	return false;
+}
+
+void myInit(){
+	//pthread_mutex_init(&mutex_, NULL);
+}
+
+int myLock(const char *str){
+	if(pthread_mutex_lock(&mutex_)!=0){
+		printf("Can't lock mutex\n");
+		return -1;
+	}
+	printf("%s locks the mutex\n", str);
+	getch();
+}
+
+void myUnlock(const char* str){
+	pthread_mutex_unlock(&mutex_);
+	printf("%s unlocks the mutex\n", str);
+	getch();
+}
+
+void reader(char *name){
+	char str[100], buf[1000];
+
+	for(;;){
+		clearStr(str, 100);
+		clearStr(buf, 1000);
+
+		myLock(name);
+
+		if(readLine(rfd, str))
+			printf("My name is \"%s\", and i have read flowing string : \"%s\"\n", name, str);
+		else
+			printf("My name is \"%s\", and i can't read string, becouse of EOF\n", name);
+
+		myUnlock(name);
+		usleep(rand()%100000);
+	}
+
+	return;
+}
+
+void writeInfoLine(int wfd, const char *str){
+	char buf[100];
+	clearStr(buf, 100);
+	sprintf(buf, "I am the main thread and i have write this :%s, %s\n", getTime(), str);
+	write(wfd, buf, strlen(buf));
+}
+
+void* thfunc(void* arg){
+	reader((char*)(arg));
+}
+
+int main(){
+	int N=10;
+
+	pthread_t threads[N];
+	char names[10][10]={};
+	wfd = open("file", O_RDWR|O_CREAT|O_TRUNC, 0666); 
+	if(wfd==-1){
+		printf("can't open file for write\n");
+		return -1;
+	}
+
+	rfd = open("file", O_RDWR|O_CREAT|O_TRUNC, 0666); 
+	if(rfd==-1){
+		printf("can't open file for read\n");
+		return -1;
+	}
+
+
+	for(int i=0;i<N;i++){
+		sprintf(names[i], "%d", i);
+		pthread_create(&threads[i], 0, thfunc, (void*)names[i]);
+	}
+	int lineNumber=0;
+	for(int i=0;i<50;i++){
+		myLock("main");
+		for(int j=0, r=rand()%10 + 4;j<r;j++){
+			char buf[30]={};
+			sprintf(buf, "line is %d", lineNumber++);
+			writeInfoLine(wfd, buf);
+		}
+		myUnlock("main");
+		usleep(21000);
+	}
+	usleep(1000000);
+	return 0;
+}
+
---
+++ lab7/main.cpp
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <iostream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+#include <termios.h>
+
+#include <semaphore.h>
+
+using namespace std;
+
+pthread_rwlock_t rw_lock;
+
+int wfd, rfd;
+
+
+char getch(){
+
+	char buf=0;
+	struct termios old={0};
+	fflush(stdout);
+	if(tcgetattr(0, &old)<0)
+		perror("tcsetattr()");
+	old.c_lflag&=~ICANON;
+	old.c_lflag&=~ECHO;
+	old.c_cc[VMIN]=1;
+	old.c_cc[VTIME]=0;
+	if(tcsetattr(0, TCSANOW, &old)<0)
+		perror("tcsetattr ICANON");
+	if(read(0,&buf,1)<0)
+		perror("read()");
+	old.c_lflag|=ICANON;
+	old.c_lflag|=ECHO;
+	if(tcsetattr(0, TCSADRAIN, &old)<0)
+		perror ("tcsetattr ~ICANON");
+	//printf("%c\n",buf);
+	return buf;
+}
+
+char * getTime(){
+	time_t current_time;
+	struct tm * time_info;
+	static char timeString[9];  // space for "HH:MM:SS\0"
+
+	time(&current_time);
+	time_info = localtime(&current_time);
+
+	strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info);
+	return timeString;
+}
+
+
+void clearStr(char* str, int len){
+	for(int i=0;i<len;i++){
+		str[i]=0;
+	}
+	return;
+}
+
+bool readLine(int fd, char *str){
+	char ch=0;
+	for(;read(fd, &ch, 1)!=0;str++){
+		if(ch=='\n')return true;
+		*str=ch;
+	}
+	return false;
+}
+
+void myInit(){
+	pthread_rwlock_init(&rw_lock, NULL);
+}
+
+int myLock(const char *str){
+	pthread_rwlock_rdlock(&rw_lock);
+	printf("%s locks the RW\n", str);
+	getch();
+}
+
+void myUnlock(const char* str){
+	pthread_rwlock_unlock(&rw_lock);
+	printf("%s unlocks the RW\n", str);
+	getch();
+}
+
+void reader(char *name){
+	char str[100], buf[1000];
+
+	for(;;){
+		clearStr(str, 100);
+		clearStr(buf, 1000);
+		usleep(rand()%100000);
+		myLock(name);
+
+		if(readLine(rfd, str))
+			printf("My name is \"%s\", and i have read flowing string : \"%s\"\n", name, str);
+		else
+			printf("My name is \"%s\", and i can't read string, becouse of EOF\n", name);
+
+		myUnlock(name);
+		usleep(rand()%500000);
+	}
+
+	return;
+}
+
+void writeInfoLine(int wfd, const char *str){
+	char buf[100];
+	clearStr(buf, 100);
+	sprintf(buf, "I am the main thread and i have write this :%s, %s\n", getTime(), str);
+	write(wfd, buf, strlen(buf));
+}
+
+void* thfunc(void* arg){
+	reader((char*)(arg));
+}
+
+int main(){
+	int N=10;
+	myInit();
+	pthread_t threads[N];
+	char names[10][10]={};
+	wfd = open("file", O_RDWR|O_CREAT|O_TRUNC, 0666); 
+	if(wfd==-1){
+		printf("can't open file for write\n");
+		return -1;
+	}
+
+	rfd = open("file", O_RDWR|O_CREAT|O_TRUNC, 0666); 
+	if(rfd==-1){
+		printf("can't open file for read\n");
+		return -1;
+	}
+
+	//pthread_rwlock_wrlock(&rw_lock);
+	for(int i=0;i<N;i++){
+		sprintf(names[i], "%d", i);
+		pthread_create(&threads[i], 0, thfunc, (void*)names[i]);
+	}
+	int lineNumber=0;
+	for(int i=0;i<10000;i++){
+		pthread_rwlock_wrlock(&rw_lock);
+		printf("Main locks the RW\n");
+		getch();
+		for(int j=0, r=rand()%10 + 4;j<r;j++){
+			char buf[30]={};
+			sprintf(buf, "line is %d", lineNumber++);
+			writeInfoLine(wfd, buf);
+		}
+		pthread_rwlock_unlock(&rw_lock);
+		printf("Main unLocks the RW\n");
+		usleep(21000);
+	}
+	usleep(1000000);
+	return 0;
+}
+
